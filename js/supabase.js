85c29a838daf34a3bc05315572d7a6f157734704
FeriurGuy <feriramadhanazhari@gmail.com>
2026-02-06 01:31:45 +0700

    Security: Stop tracking API keys

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-        
-const SUPABASE_URL = 'https://urrpvfcqnkosawceesui.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycnB2ZmNxbmtvc2F3Y2Vlc3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxOTQyMjgsImV4cCI6MjA4Mjc3MDIyOH0.QVUJqeaoJJY3VU7dtm515kerVF6zBm0k9jJeW2zClqo';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-console.log("Supabase Client Ready!");
