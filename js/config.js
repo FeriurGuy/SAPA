1ec1ec100bcbd2690bf0f863be9ccce648d1d689
FeriurGuy <feriramadhanazhari@gmail.com>
2026-02-06 03:16:54 +0700

    remove gitignore

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// --- BRANKAS RAHASIA ---
+
+// 1. Kunci Gemini (Untuk Modul Cerdas AI)
+export const GEMINI_KEY = "AIzaSyDZPQqnizHpJ2Fs7k11bPtJ2Ndr5cAHzmI";
+
+// 2. Kunci Supabase (Untuk Login & Database)
+// Pastikan URL dan KEY ini ada di sini!
+export const SUPABASE_URL = 'https://urrpvfcqnkosawceesui.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycnB2ZmNxbmtvc2F3Y2Vlc3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxOTQyMjgsImV4cCI6MjA4Mjc3MDIyOH0.QVUJqeaoJJY3VU7dtm515kerVF6zBm0k9jJeW2zClqo';
