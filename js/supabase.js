9404849bf9d3b1ad8c1c13d38c128961489f397f
FeriurGuy <feriramadhanazhari@gmail.com>
2026-02-06 03:14:04 +0700

    supabase on ol

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,13 @@
+// Import Client Supabase
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
+// Import Kunci dari config.js
 import { SUPABASE_URL, SUPABASE_KEY } from './config.js';
 
+// Cek di Console apakah kunci terbaca (Bisa dihapus nanti)
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+    console.error("❌ Gawat! Kunci Supabase tidak ditemukan di config.js");
+}
+
+// Buat Koneksi
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
